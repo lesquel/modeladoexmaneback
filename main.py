9ae5830e5e56fae559b806f9a34c1c6eec106c11
lesquel <lesquel1319@gmail.com>
2025-06-15 21:17:17 -0500

    first commit

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from agno.playground import Playground
 from agno.storage.sqlite import SqliteStorage
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.tools.yfinance import YFinanceTools
-
 agent_storage: str = "tmp/agents.db"
-
 web_agent = Agent(
-    name="Miquel Agent for Web",
+    name="Pedro Agent for Web",
     model=Groq(id="llama-3.3-70b-versatile"),
     tools=[DuckDuckGoTools()],
-    instructions=["You always referred to the user as Miquel Muñiz"],
+    instructions=["You always referred to the user as Pedro"],
     # Store the agent sessions in a sqlite database
     storage=SqliteStorage(table_name="web_agent", db_file=agent_storage),
     # Adds the current date and time to the instructions
@@ -23,21 +21,18 @@ web_agent = Agent(
     # Adds markdown formatting to the messages
     markdown=True,
 )
-
 finance_agent = Agent(
-    name="Miquel Agent for Finance",
+    name="Pedro Agent for Finance",
     model=Groq(id="llama-3.3-70b-versatile"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
-    instructions=["You always referred to the user as Miquel Muñiz"],
+    instructions=["You always referred to the user as Pedro"],
     storage=SqliteStorage(table_name="finance_agent", db_file=agent_storage),
     add_datetime_to_instructions=True,
     add_history_to_messages=True,
     num_history_responses=5,
     markdown=True,
 )
-
 playground_app = Playground(agents=[web_agent, finance_agent])
 app = playground_app.get_app()
-
 if __name__ == "__main__":
     playground_app.serve("main:app", reload=True)
